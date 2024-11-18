a3fbd23b7b18a71851e4f141478447390292503f
arakhamia81 <nikoloziarakhamia@gmail.com>
2024-11-18 15:20:38 +0000

    second commit

---
+++ level 07/classwork/classwork.py
@@ -0,0 +1,22 @@
+name=input("შეიტანე შენი სახელი")
+number=input("შეიტანე შენი რიცხვი")
+print(name, number)
+
+"num1 = 4"
+
+"num2 = 15"
+
+"num3 = 10"
+
+"num4 = 6"
+
+
+print("num1 + 34")
+print("num2 - 5")
+print("num3 * 2")
+print("num4 / 3")
+
+
+#name = niko
+
+#number = 12
